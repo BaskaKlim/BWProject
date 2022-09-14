2f8d4f2a68306f99e1acd71b6da2e07285bdd34f
baskaklimek <baska.klimekova@gmail.com>
2022-09-14 20:19:48 +0200

    initizialing springboot project

---
+++ src/main/java/com/bwproject/grouptwo/GrouptwoApplication.java
@@ -0,0 +1,13 @@
+package com.bwproject.grouptwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouptwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrouptwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bwproject/grouptwo/GrouptwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwproject.grouptwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouptwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
